38acdbfb49897437f42dba5ea15f2366004a6972
Arthur Chang <arthurc0102@gmail.com>
2020-12-30 16:48:35 +0800

    Add todo comment

--- config.example.py
+++ config.example.py
@@ -3,6 +3,7 @@ username = 'admin-user'
 password = 'admin-password'
 
 # Choices
+# TODO: `user_roles`, `affiliation`, `category` and Use name not id.
 enabled = '1'  # Enable
 user_roles = '3'  # Team member
 affiliation = '1'  # Taiwan
