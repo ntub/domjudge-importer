74bfed5257d51b4c88689e8e7e4adf40354d716b
Arthur Chang <arthurc0102@gmail.com>
2020-12-30 16:49:27 +0800

    Fix spelling error

--- config.example.py
+++ config.example.py
@@ -3,7 +3,7 @@ username = 'admin-user'
 password = 'admin-password'
 
 # Choices
-# TODO: `user_roles`, `affiliation`, `category` and Use name not id.
+# TODO: `user_roles`, `affiliation` and `category` use name to config not id.
 enabled = '1'  # Enable
 user_roles = '3'  # Team member
 affiliation = '1'  # Taiwan
